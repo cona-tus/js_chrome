ee062172888dbf4c681eba16417741c298039489
cona-tus <cam_panella@naver.com>
2021-10-14 15:19:05 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,24 @@
 const toDoform = document.getElementById("todo-form");
 // const toDoInput = document.querySelector("#todo-form input"); 과 같다.
 const toDoInput = toDoform.querySelector("input");
+// 아래는 ul
 const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+
+}
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -20,3 +31,5 @@ function handleTodoSubmit(event){
 }
 
 toDoform.addEventListener("submit", handleTodoSubmit);
+
+
