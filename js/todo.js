361fb9dcb89e391753af5af1562fa59414168ebb
cona-tus <cam_panella@naver.com>
2021-10-14 20:40:08 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -4,10 +4,17 @@ const toDoInput = toDoform.querySelector("input");
 // 아래는 ul
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("TODOS_KEY", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
-
 }
 
 function paintToDo(newTodo){
@@ -27,9 +34,16 @@ function handleTodoSubmit(event){
     // input의 현재 value를 새로운 변수에 복사하는 것. 그 다음 무얼하건 newTodo와는 관계 없음.
     const newTodo = toDoInput.value;
     toDoInput.value ="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    savedToDos();
 }
 
 toDoform.addEventListener("submit", handleTodoSubmit);
 
+const savedTodos = localStorage.getItem(TODOS_KEY);
 
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the trun of ", item));
+}
