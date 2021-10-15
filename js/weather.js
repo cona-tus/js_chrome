4a7d36eecad3870ff607bee9690790ef7d386b5e
cona-tus <cam_panella@naver.com>
2021-10-15 22:01:59 +0900

    JS : weather

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,12 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "2b14f69a01b2b4c005af95061c6ae2ea";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
