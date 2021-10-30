f3959c4d86206598b30d019e27163e560109eaef
cona-tus <cam_panella@naver.com>
2021-10-30 16:16:05 +0900

    HTML, CSS : add mobile

--- js/background.js
+++ js/background.js
@@ -1,20 +1,11 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg",
- ];
+const images = ["1.png", "2.png", "3.png"];
 
- const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
- const bgImage = document.createElement("img");
+const bgImage = document.createElement("img");
 
- bgImage.src = `img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
- document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);
+
+bgImage.id = "bgImage";
